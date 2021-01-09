1034e4ab894bd58c201defcbdfaed07c50d9bc67
jiniusz <dev.jiniusz@gmail.com>
2021-01-09 17:44:38 +0700

    adding Navbar

---
+++ src/components/structure/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+<nav class="z-depth-0 navbar">
+    <div class="nav-wrapper white">
+        <a href="#" class="brand-logo center grey-text text-darken-1">Jiniusz</a>
+        <ul id="nav-mobile" class="menu left hide-on-med-and-down">
+            <li><a href="" class="grey-text text-darken-1">WORK</a></li>
+            <li><a href="" class="grey-text text-darken-1">ABOUT</a></li>
+            <li class="menu-active "><a href="" class="grey-text text-darken-1">CONTACT</a></li>
+        </ul>
+        <ul id="nav-mobile" class="contract right hide-on-med-and-down">
+            <li><a href="" class="grey-text text-darken-1"><i class="fab fa-github fa-lg"></i></a></li>
+            <li><a href="" class="grey-text text-darken-1"><i class="fab fa-facebook fa-lg"></i></a></li>
+            <li><a href="" class="grey-text text-darken-1"><i class="fab fa-instagram fa-lg"></i></a></li>
+        </ul>
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.navbar{
+    margin-top: 65px;
+    line-height: 30px;
+}
+
+.navbar .nav-wrapper ul.menu {
+    margin-left: 132px;
+    margin-top: 20px;
+}
+.navbar .nav-wrapper ul li.menu-active a {
+    position: absolute;
+    display: inline;
+    border-bottom: 1px solid #757575;
+}
+
+.navbar .nav-wrapper .center{
+    font-size: 1.2em;
+    margin-top: 20px;
+}
+
+.navbar .nav-wrapper ul.contract {
+    margin-right: 132px;
+}
+.navbar .nav-wrapper ul li a{
+    font-size: 0.82em;
+    font-weight: 520;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <Navbar/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/structure/Navbar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
