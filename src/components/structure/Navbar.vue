5da51dfa3df959a4a1155cde374c220f203a678b
jiniusz <dev.jiniusz@gmail.com>
2021-01-09 18:06:24 +0700

    adding router-link

--- src/components/structure/Navbar.vue
+++ src/components/structure/Navbar.vue
@@ -3,9 +3,9 @@
     <div class="nav-wrapper white">
         <a href="#" class="brand-logo center grey-text text-darken-1">Jiniusz</a>
         <ul id="nav-mobile" class="menu left hide-on-med-and-down">
-            <li><a href="" class="grey-text text-darken-1">WORK</a></li>
-            <li><a href="" class="grey-text text-darken-1">ABOUT</a></li>
-            <li class="menu-active "><a href="" class="grey-text text-darken-1">CONTACT</a></li>
+            <li><router-link to="" class="grey-text text-darken-1">WORK</router-link></li>
+            <li><router-link to="" class="grey-text text-darken-1">ABOUT</router-link></li>
+            <li class="menu-active "><router-link to="" class="grey-text text-darken-1">CONTACT</router-link></li>
         </ul>
         <ul id="nav-mobile" class="contract right hide-on-med-and-down">
             <li><a href="" class="grey-text text-darken-1"><i class="fab fa-github fa-lg"></i></a></li>
@@ -28,6 +28,10 @@ export default {
     line-height: 30px;
 }
 
+.navbar .nav-wrapper ul li a{
+    font-size: 0.82em;
+    font-weight: 520;
+}
 .navbar .nav-wrapper ul.menu {
     margin-left: 132px;
     margin-top: 20px;
@@ -45,9 +49,5 @@ export default {
 
 .navbar .nav-wrapper ul.contract {
     margin-right: 132px;
-}
-.navbar .nav-wrapper ul li a{
-    font-size: 0.82em;
-    font-weight: 520;
 }
 </style>
